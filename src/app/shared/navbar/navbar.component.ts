3ec252301cc6476fd926c6aea2187ba1969837e8
Nicolas D'Addabbo <nico.daddabbo7100@gmail.com>
2023-03-16 20:05:13 +0100

    style navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -5,31 +5,32 @@ import { BookService } from '../services/book.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  isAdmin: boolean = false
+  whereWeAre: string = 'books';
+  isAdmin: boolean = false;
   isConnected: boolean = false;
   isSwitch: boolean = false;
-  constructor(private _authService : AuthService){}
+  constructor(private _authService: AuthService) {}
 
   ngOnInit() {
     this._authService.isAdmin.subscribe((state: boolean) => {
       this.isAdmin = state;
       this._authService.isConnected.subscribe((state: boolean) => {
         this.isConnected = state;
-      })
-    })
+      });
+    });
   }
 
   switch(): void {
     this.isSwitch === true ? false : true;
-
   }
 
-  connect(): void {
-   
+  connect(): void {}
+
+  isActive(state: string) {
+    this.whereWeAre = state;
+    console.log(this.whereWeAre);
   }
-
-
 }
